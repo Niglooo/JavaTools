74a7acfcbb1dbc25d45558bda10944e1d488cbc0
Niglooo <nigloo@gmx.fr>
2020-07-21 00:08:17 +0200

    Add module-info.java, pom.xml and eclipse project files

--- src/module-info.java
+++ src/module-info.java
@@ -1,4 +1,19 @@
-module Injection
-{
+open module nigloo.tools {
 	requires java.logging;
+	
+	requires transitive javafx.base;
+	requires transitive javafx.controls;
+	requires javafx.fxml;
+	requires transitive javafx.graphics;
+	
+	requires transitive com.google.gson;
+
+	exports nigloo.tool;
+	exports nigloo.tool.collection;
+	exports nigloo.tool.gson;
+	exports nigloo.tool.injection;
+	exports nigloo.tool.injection.annotation;
+	exports nigloo.tool.injection.impl;
+	exports nigloo.tool.javafx.component;
+	exports nigloo.tool.thread;
 }
